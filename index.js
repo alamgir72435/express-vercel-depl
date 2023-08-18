539d6fa357b4320fcc976327855d6e5c5f6230fd
Alamgir Ahamed <alamgir72435@gmail.com>
2023-08-18 20:05:44 +0600

    root req with json placeholder todos

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const app = require('express')();
+const { default: axios } = require('axios');
 const { v4 } = require('uuid');
+
+var todos = null;
+
+app.get('/', async (req, res) => {
+
+  if (!todos) {
+    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/todos`);
+    todos = data;
+  }
+  res.json(todos);
+})
+
 
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
@@ -8,9 +21,15 @@ app.get('/api', (req, res) => {
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
+
+
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
+
+app.listen(5000, () => {
+  console.log(`server running on 5000 `);
+})
 
 module.exports = app;
